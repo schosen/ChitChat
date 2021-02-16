049a5e3c69b31cdd968ef5363fd38da087434d52
Sarah <sarahchosen@hotmail.co.uk>
2021-02-16 14:12:18 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root to: "home#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  get 'home/index'
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
