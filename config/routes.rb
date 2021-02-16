612f1b368ab666dee9283cb58716b5cc0e7043dc
Sarah <sarahchosen@hotmail.co.uk>
2021-02-16 14:35:57 +0000

    added room / message routes added view for room

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :room_messages
   resources :rooms
-  root to: "home#index"
+  # root to: "home#index"
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
+
+  root controller: :rooms, action: :index
+
+  resources :room_messages
+  resources :rooms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
