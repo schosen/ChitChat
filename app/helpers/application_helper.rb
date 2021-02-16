916aa6b9bdbdcd7217ecb1962237811d6900987c
Sarah <sarahchosen@hotmail.co.uk>
2021-02-16 17:59:04 +0000

    created avatar for new users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def profile_pic(user, size)
+    url = "https://picsum.photos/id/#{1000 + user.id}/#{size}/#{size}"
+  end
+
+  def error_profile_pic
+    url = "https://www.kirkham-legal.co.uk/wp-content/uploads/2017/02/profile-placeholder.png"
+  end
+
 end
