2cc07a44b20f7474c66927e1d9ed465c24b034cd
Roury Hinds <roury_hinds@outlook.com>
2021-02-23 12:19:20 +0000

    styling updates to profile page, added navbar dropdown links, added back-en functionality for users to add bio, frontend work done for bio to display on profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_141900) do
+ActiveRecord::Schema.define(version: 2021_02_22_212046) do
 
   create_table "room_messages", force: :cascade do |t|
     t.integer "room_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_141900) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
